2213c5c207cda200f8cea9d25320569ddee79710
talat4111 <qureshitalatfatima@gmail.com>
2026-02-13 12:29:49 +0530

    fix vite allowedHosts

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,11 @@
-﻿import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+﻿import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()]
-});
+  plugins: [react()],
+  preview: {
+    host: true,
+    port: 10000,
+    allowedHosts: ['.onrender.com']
+  }
+})
